b5cb54b645b924db47c2ca14dc78948a24159ffd
seguvega <seguvega@hotmail.com>
2021-05-18 15:15:22 -0500

    Usuario

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,27 @@ Router.get("/", (req, res) => {
 
 Router.get("/user", isAutentificado,(req, res) => {
     var tempObj = req.user;
-    var tempObjSinId = req.user;
     console.log(tempObj);
     res.render("menu_us", {
-        data: tempObj,
-        id: tempObj._id
+        id: tempObj._id,
+        name: tempObj.name,
+        cedula: tempObj.cedula,
+        mobile: tempObj.mobile,
+        direccion: tempObj.direccion,
+        correo:tempObj.correo,
+        date: tempObj.date,
+        cod: tempObj.cod,
+        user: tempObj.user,
+        sexo: tempObj.sexo,
+        tSangre: tempObj.tSangre,
+        eCivil: tempObj.eCivil,
+        etnia: tempObj.etnia,
+        passconf: tempObj.passconf,
+        clima: tempObj.clima,
+        cole: tempObj.cole,
+        color: tempObj.color,
+        pais: tempObj.pais,
+        peli_serie: tempObj.peli_serie,
     });
 });
 
